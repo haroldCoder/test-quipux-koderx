d667d7e196e22dd4be58b0710934c14fd3ec3f16
haroldCoder <haroldc2005@gmail.com>
2024-09-17 16:26:22 -0500

    create article component & move menulat component to components folder

--- src/app/components/article/article.component.spec.ts
+++ src/app/components/article/article.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MenulatComponent } from './menulat.component';
+import { ArticleComponent } from './article.component';
 
-describe('MenulatComponent', () => {
-  let component: MenulatComponent;
-  let fixture: ComponentFixture<MenulatComponent>;
+describe('ArticleComponent', () => {
+  let component: ArticleComponent;
+  let fixture: ComponentFixture<ArticleComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MenulatComponent]
+      imports: [ArticleComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(MenulatComponent);
+    fixture = TestBed.createComponent(ArticleComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
