76de2f3ed0d52bae69f81ff8c426830b8494fa15
haroldCoder <haroldc2005@gmail.com>
2024-09-17 21:52:36 -0500

    create article view and carousel components

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from "../carousel/carousel.component";
+import { ArticleItemsComponent } from "../article-items/article-items.component";
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [CarouselComponent, ArticleItemsComponent],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
 export class ArticleComponent {
-
+  email = "usuarioapellido@hotmail.com"
 }
---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css'
+})
+
+export class CarouselComponent {
+  @Input() images : Array<any> = []
+  
+}
